39f24e588021f4256e450f84b8007e37d2264ec7
Dickachu <75149375+gi728h@users.noreply.github.com>
2023-09-25 11:23:33 +0530

    commit done

--- newsql.php
+++ newsql.php
@@ -23,6 +23,8 @@ if ($mysqli->connect_error) {
 <head>
     <meta charset="UTF-8">
     <title>MOHITO'S Paid project</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
 </head>
 
@@ -48,62 +50,77 @@ if ($mysqli->connect_error) {
     ?>
         <section>
             <!-- Columns per table -->
-            <table class="<?php echo $tablename?>" id="myTable">
-                <!-- Fields -->
-                <tr>
-                    <?php
-                    foreach ($col as $x) {
-                    ?>
-                        <th><?php echo $x; ?></th>
-                    <?php
-                    }
-                    ?>
-                </tr>
-                <!-- rows per table -->
-                <?php
-                $row_index = 0;
-                while ($row = $rows->fetch_assoc()) {
-                ?>
-                    <tr class="<?php echo $row_index; ?>">
-                        <!-- columns per row -->
+            <table class="<?php echo $tablename ?>" id="myTable">
+                <thead>
+                    <!-- Fields -->
+                    <tr>
                         <?php
                         foreach ($col as $x) {
                         ?>
-                            <td class="v <?php echo $tablename . " " . $x . " " . $row_index; ?>" contenteditable><?php echo $row[$x]; ?></td>
+                            <th><?php echo $x; ?></th>
                         <?php
                         }
                         ?>
-                        <td class="delete"><svg class="<?php echo $tablename?> delete" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                            </svg></td>
                     </tr>
-                <?php
-                    $row_index++;
-                }
-                ?>
-                
+                </thead>
+                <tbody>
+                    <!-- rows per table -->
+                    <?php
+                    $row_index = 0;
+                    while ($row = $rows->fetch_assoc()) {
+                    ?>
+                        <tr class="<?php echo $row_index; ?>">
+                            <!-- columns per row -->
+                            <?php
+                            foreach ($col as $x) {
+                            ?>
+                                <td class="v <?php echo $tablename . " " . $x . " " . $row_index; ?>" contenteditable><?php echo $row[$x]; ?></td>
+                            <?php
+                            }
+                            ?>
+                            <td><button style="height:20px; width:20px; margin:0;" class="<?php echo $tablename ?> delete"></button></td>
+                        </tr>
+                    <?php
+                        $row_index++;
+                    }
+                    ?>
+                </tbody>
             </table>
             <button class="create" style="margin-left: 48%; margin-top: 20px">Create row</button>
+            <form class="form">
+                <input type="text" placeholder="search" name="search">
+                <select id="column" name="column">
+                    <?php
+                    foreach ($col as $x) {
+                    ?>
+                        <option value="<?php echo $x ?>"><?php echo $x ?></option>
+                    <?php
+                    }
+                    ?>
+                </select>
+                <button type="button" class="search"></button>
+            </form>
 
-              <!-- <script>
-                function myCreateFunction(){
-                    var table = document.getElementById("myTable");
-                    var row = table.insertRow();
-                    var cell1 = row.insertCell(0);
-                    var cell2 = row.insertCell(1);
-                    var cell3 = row.insertCell(2);
-                    cell1.innerHTML = "";
-                    cell2.innerHTML = "";
-                    cell3.innerHTML = "";
-                }
-              </script> -->
         </section>
-        <div id="remove_cursor" contenteditable style="visibility: hidden;">
-
-        </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-        <script src="./index.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="./insert.js"></script>
+        <script src="./delete.js"></script>
+        <script src="./update.js"></script>
+        <script>
+            window.onload = function() {
+                if(sessionStorage.getItem("insert")){
+                    console.log("Record inserted successfully.")
+                    sessionStorage.clear()
+                    $("body").append('<div class="alert alert-primary" role="alert">Record inserted successfully.</div>')
+                }
+                if(sessionStorage.getItem("delete")){
+                    console.log("Record deleted successfully.")
+                    $("body").append('<div class="alert alert-primary" role="alert">Record deleted successfully.</div>')
+                    sessionStorage.clear()
+                }
+            }
+        </script>
 </body>
 
 </html>
